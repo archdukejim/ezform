a4620d7e1d5bb51d7080366bc41ace10a0f7fa17
archdukejim <84357392+archdukejim@users.noreply.github.com>
2023-07-02 02:45:13 -0400

    Update ezform.psm1
    Allow for testing of the script modules, recent commits reduce errors with module inconsistencies and allows proper loading of the modules, regardless of their completion

--- ezform.psm1
+++ ezform.psm1
@@ -1,9 +1,4 @@
 function Start-EZForm {
-	[cmdletbinding()]
-	BEGIN{}
-	PROCESS{
-		Add-Type -AssemblyName System.Windows.Forms
-		[System.Windows.Forms.Application]::EnableVisualStyles()
-	}
-	END{}
+	Add-Type -AssemblyName System.Windows.Forms
+	[System.Windows.Forms.Application]::EnableVisualStyles()
 }
